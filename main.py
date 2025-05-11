f82531688643d1e28dace8494bc219d31a9ba968
Ashish Pandey <pandaya10@gmail.com>
2025-05-11 12:10:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,19 @@ cap.set(4, 720)
 
 detector = HandDetector(detectionCon=0.8)
 
+
+class Button():
+    def __init__ (self, pos, text, size =[100,100]):
+        self.pos = pos
+        self.size = size
+        self.text = text
+
+        cv2.rectangle(img, (self.pos), self.size, (255,0,255), cv2.FILLED)
+        cv2.putText(img, self.text, (self.pos[0]+25, self.pos[1]+25), 
+                    cv2.FONT_HERSHEY_COMPLEX, 3, (255,255,255), 5)
+
+
+
 while True:
     success, img = cap.read()
     
@@ -19,6 +32,8 @@ while True:
         bboxInfo = hand["bbox"]
 
         print("Index finger tip:", lmList[8])
+
+    mubutton = Button([100,100], "Q")
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
