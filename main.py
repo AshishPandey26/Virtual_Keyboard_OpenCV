35a165b151fd8e87b82abe240f810c671ea885e1
Ashish Pandey <pandaya10@gmail.com>
2025-05-11 22:29:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,11 +13,12 @@ class Button():
         self.pos = pos
         self.size = size
         self.text = text
-
+    
+    def draw(self, img):
         cv2.rectangle(img, (self.pos), self.size, (255,0,255), cv2.FILLED)
         cv2.putText(img, self.text, (self.pos[0]+25, self.pos[1]+25), 
                     cv2.FONT_HERSHEY_COMPLEX, 3, (255,255,255), 5)
-
+        return img
 
 
 while True:
